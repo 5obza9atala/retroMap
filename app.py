af7c5f44696c09d8a3c0b7e59a2e8d2f51a51e0e
5obza9atala <koueraaymane2004@gmail.com>
2026-08-24 01:11:41 +0200

    Update app.py
    V2 with slider allowing you to change the radius

--- app.py
+++ app.py
@@ -3,17 +3,22 @@ import prettymaps
 import osmnx as ox
 import matplotlib.pyplot as plt
 
-# Swapped to a secondary main global mirror
-ox.settings.overpass_endpoint = 'https://lz4.overpass-api.de/api/interpreter'
+# The reliable global mirror
+ox.settings.overpass_endpoint = 'https://overpass.nchc.org.tw/api/interpreter'
+ox.settings.timeout = 180 # Give the server more time to respond
 
 st.title("🗺️ Custom Retro Map Generator")
 
+# Interactive Widgets
 location = st.text_input("Enter a City or Coordinates:", "Chefchaouen, Morocco")
+# Add a slider so you can control how heavy the map is!
+map_radius = st.slider("Map Radius (Meters) - Keep under 500 for faster loading:", 100, 1500, 300)
 
 if st.button("Generate Map"):
-    with st.spinner("Drawing map... this may take a minute!"):
+    with st.spinner("Drawing map... smaller radiuses load much faster!"):
         try:
-            plot = prettymaps.plot(location, radius=800, preset='heerhugowaard')
+            # Use the new slider variable
+            plot = prettymaps.plot(location, radius=map_radius, preset='heerhugowaard')
             
             fig = plt.gcf()
             st.pyplot(fig)
